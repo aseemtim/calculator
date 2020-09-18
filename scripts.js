9ab70824ddcd9151e5bb832654d5a23c3bbb01e9
Aseem <aseem.tim@gmail.com>
2020-09-18 16:07:57 +0545

    Added logic to get total

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-function add(a,b) {
+function sum(a,b) {
     return a+b;
 }
 
@@ -14,22 +14,65 @@ function divide(a,b) {
     return a/b;
 }
 
-function operate(operator,a,b) {
-    return operator(a,b);
+function operate(calculate,a,b) {
+    if(calculate=="add"){
+        return sum(a,b)
+    }
+    if(calculate=="subtract"){
+        return subtract(a,b)
+    }
+    if(calculate=="multiply"){
+        return multiply(a,b)
+    }
+    if(calculate=="divide"){
+        return divide(a,b)
+    }
 }
 
-function displayValue() {
-
-}
 
 let displayItems = document.querySelector('.displayItems');
 
-let buttonValue;
+let displayValue="";
+let collectInput="";
+let total=0;
+let operator;
+let operatorCounter=0;
+let afterSplit;
+let firstNumber;
+let secondNumber;
 
 let buttons = document.querySelectorAll('button');
 buttons.forEach(button => {
     button.addEventListener('click', () => {
-        buttonValue=button.textContent;
-        displayItems.textContent=buttonValue;
+        
+        // console.log(collectInput);
+        if(button.className=='number'){
+            displayValue+=button.textContent;
+            displayItems.textContent=displayValue;
+            collectInput+=button.textContent;
+        }
+
+        if(button.className=='operators') {
+            operator=button.id;
+            collectInput+=operator;
+            displayValue="";
+            operatorCounter++;
+        }
+        afterSplit = collectInput.split(operator);
+        if(afterSplit[1]&&!afterSplit[2]) {
+            firstNumber=Number(afterSplit[0].toString());
+            secondNumber=Number(afterSplit[1].toString());
+            total=operate(operator,firstNumber,secondNumber);
+            operatorCounter=0;
+        }
+        
+        console.log(operatorCounter,firstNumber,secondNumber,total);
     })
 });
+
+
+//1.display input on the screen.
+    //show input until operator is pressed
+//2.operator should not be shown.
+//3.After operator is pressed next input should be shown.
+//4.Equals should return the answer.
