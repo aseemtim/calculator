e1cf8689107133607e893f8c5d64e2e769222808
Aseem <aseem.tim@gmail.com>
2020-09-20 20:11:05 +0545

    Everything is done except % bug and overflow

--- scripts.js
+++ scripts.js
@@ -41,60 +41,99 @@ let displayItems = document.querySelector('.displayItems');
 
 let displayValue="";
 let collectInput="";
-let total=0;
+let total;
 let operator;
 let operatorCounter=0;
 let afterSplit;
 let firstNumber;
 let secondNumber;
+let checkPosition;
 
 let buttons = document.querySelectorAll('button');
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         
         // console.log(collectInput);
+        //display input value and collect it in a string variable.
         if(button.className=='number'){
+            total=null; //so that if the input is number after equals the display shows current input
             displayValue+=button.textContent;
             displayItems.textContent=displayValue;
             collectInput+=button.textContent;
         }
-
+        //operators gives total if operatorCounter is more than 2
         if(button.className=='operators') {
-            console.log(typeof Number(collectInput[collectInput.length-1]))
+            //total gets its value if the operatorCounter is 2 or the input is equals
+            if(total){
+                collectInput=total.toString(); //for storing total in collectInput if the input is operator after equals
+            }
             if(typeof Number(collectInput[collectInput.length-1])=="number"){
                 operatorCounter++;
                 if(operatorCounter==2) {
                     total=splitAndOperate();
-                    displayItems.textContent=total;
+                    displayItems.textContent=total.toString();
                     operatorCounter--;
-                    collectInput=total.toString();
-                    displayValue="";
+                    collectInput=total.toString();//store total in collectString when there is continous use of operators.
+                    displayValue=""; //reset so that the number after an operator would not concat to the previous display value.
                 }
                 operator=button.id;
                 collectInput+=operator;
                 displayValue="";
             }
         }
-
+        //equals calculates current info and displays it
         if(button.id=='equals') {
-            if(typeof Number(collectInput[collectInput.length-1])=="number"){
-                total=splitAndOperate();
-                displayItems.textContent=total; 
-                collectInput=total.toString();
-                operatorCounter--;
-                
+            if(collectInput){
+                if(collectInput.includes(operator)){
+                    if(typeof Number(collectInput[collectInput.length-1])=="number"){ //checks if the last value of array is number
+                        if(collectInput.split(operator).pop()=="0"){ //pop() returs the popped item
+                            displayItems.textContent="YUDoThis?:(";
+                        }else{    
+                        total=splitAndOperate();
+                            displayItems.textContent=total.toString(); 
+                            operatorCounter--;
+                            displayValue="";
+                            collectInput=""; 
+                            console.log(typeof total);   
+                        }
+                    }
+                }
             }   
         }
-
+        //allclear resets everything
         if(button.id=='allclear'){
             collectInput="";
-            operator="";
-            operatorCounter=0;
+            operator=null;
+            operatorCounter=null;
+            afterSplit=null;
+            checkPosition=null;
+            total=null;
             displayValue="";
             displayItems.textContent="";
         }
 
-        console.log(operator,collectInput,afterSplit,total);
+        if(button.id=='backspace'){
+            collectInput = collectInput.slice(0,collectInput.length-1);
+            displayValue=collectInput.split(operator).pop();
+            displayItems.textContent=displayValue;
+        }
+
+        //percent button should evaluate the percent value of the number and return it to the collectInput
+        if(button.id=='percent'){
+            if(collectInput){
+                if(collectInput.includes(operator)){
+                    console.log("true");
+                    checkPosition = collectInput.split(operator);
+                    if(checkPosition[1]!=""){
+                        collectInput=checkPosition[0]+operator+(Number(checkPosition[1])/100).toString();
+                    }
+                }else{
+                    collectInput=(Number(collectInput)/100).toString();
+                }
+            }
+        } 
+
+        console.log(operator,collectInput,afterSplit,checkPosition,total);
     })
 });
 
